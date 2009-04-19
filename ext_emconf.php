409dbf64c17109eb2ed488ff185212fa0a85ee90
t33k <t33k@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-19 19:16:13 +0000

    Changing the ext name to coago all over the code.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/coago/trunk@19193 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "newcachecobj"
+# Extension Manager/Repository config file for ext: "coago"
 #
 # Auto generated 05-04-2009 19:23
 #
@@ -11,17 +11,17 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'New content element COA_GO. For better caching.',
-	'description' => 'It allows to use new CE - COA_GO.',
+	'title' => 'COA_GO - per cObj caching with cache period',
+	'description' => 'This extension improves caching of content objects in TYPO3 so it can increase time rendering of page and overall performance of your site. Tags: COA_CACHED, COA_INT_CACHED, COA_INT_CACHED_AJAX',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.0.1',
+	'version' => '0.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'lockType' => '',
 	'author' => 'Krystian Szymukowicz',
 	'author_email' => 'http://typo3.prolabium.com',
-	'author_company' => '',
+	'author_company' => 'http://typo3.prolabium.com',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'constraints' => array(
