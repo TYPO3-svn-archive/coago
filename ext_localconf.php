fb14b74c8748b74cf7451241cb6cf5bee43554dd
t33k <t33k@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-19 19:16:24 +0000

    Changing the ext name to coago all over the code.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/coago/trunk@19194 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,17 +5,15 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 if (TYPO3_MODE == 'FE') {
 
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['cObjTypeAndClass'][] = 
-	array('COA_GO', 'EXT:newcachecobj/class.tx_newcachecobj.php:&tx_newcachecobj');		
+	array('COA_GO', 'EXT:coago/class.tx_coago.php:&tx_coago');		
 		
 }
 
 if (TYPO3_MODE == 'BE') {
 
-// TO DO
-
-	//$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:newcachecobj/class.tx_newcachecobj_tcemainproc.php:tx_newcachecobj_tcemainproc';
-	
-	//$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = 'EXT:newcachecobj/class.tx_newcachecobj_tcemainproc.php:tx_newcachecobj_tcemainproc';
+	// TODO
+	//$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:coago/class.tx_coago_tcemainproc.php:tx_coago_tcemainproc';
+	//$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = 'EXT:coago/class.tx_coago_tcemainproc.php:tx_coago_tcemainproc';
 
 }
 
