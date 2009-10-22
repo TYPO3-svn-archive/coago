1625c609963c85dedd4e95b58f744273a36d6d30
t33k <t33k@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-22 10:49:10 +0000

    * request method in ajax call changed from POST to GET. With POST method there was some problems on Safari at MAC (many thanks to Kurt Knote for fighting with this bug!) * fixed bug in beforeCache_db. No assign to $content after get from cache. * asHtmlComments option in debug. Allows to test the solution on production servers.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/coago/trunk@25739 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "coago"
 #
-# Auto generated 14-09-2009 10:09
+# Auto generated 22-10-2009 12:38
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -11,11 +11,11 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'COA_GO - per content object caching with cache period supported',
-	'description' => 'This extension improves caching of content objects in TYPO3 so it can decrease time rendering of page and improve overall performance of your site.',
+	'title' => 'COA_GO - per cObject caching with cache period',
+	'description' => 'This extension improves caching of content objects in TYPO3. It can decrease time rendering of page and improve overall performance of your site.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.1.4',
+	'version' => '0.1.5',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"b13b";s:18:"class.tx_coago.php";s:4:"0d0d";s:21:"ext_conf_template.txt";s:4:"a3cd";s:17:"ext_localconf.php";s:4:"7f84";s:14:"ext_tables.php";s:4:"04f4";s:10:"readme.txt";s:4:"1ca7";s:17:"readme_coa_go.gif";s:4:"d09c";s:14:"doc/manual.sxw";s:4:"6a33";s:27:"misc/class.tx_coago_mod.php";s:4:"8b68";s:14:"res/ajax/1.gif";s:4:"7b97";s:20:"static/constants.txt";s:4:"8638";s:16:"static/setup.txt";s:4:"4d39";s:37:"_later/class.tx_coago_tcemainproc.php";s:4:"e4a5";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"4c5f";s:18:"class.tx_coago.php";s:4:"e38e";s:21:"ext_conf_template.txt";s:4:"a3cd";s:17:"ext_localconf.php";s:4:"7f84";s:14:"ext_tables.php";s:4:"04f4";s:10:"readme.txt";s:4:"1ca7";s:17:"readme_coa_go.gif";s:4:"d09c";s:14:"doc/manual.sxw";s:4:"6a33";s:27:"misc/class.tx_coago_mod.php";s:4:"8b68";s:14:"res/ajax/1.gif";s:4:"7b97";s:20:"static/constants.txt";s:4:"8638";s:16:"static/setup.txt";s:4:"4d39";}',
 );
 
 ?>
